f2e1fde2a4a7802d818a8d531fe4d4a66b519c59
Rafael Guerra <faelwar@gmail.com>
2016-05-14 13:35:00 -0500

    Week 7.6

--- grails-app/migrations/week-5A.groovy
+++ grails-app/migrations/week-5A.groovy
@@ -1,10 +1,6 @@
 databaseChangeLog = {
 
-<<<<<<< HEAD
     changeSet(author: "Rafael (generated)", id: "1461960710217-1") {
-=======
-    changeSet(author: "Rafael (generated)", id: "1461944303612-1") {
->>>>>>> refs/remotes/origin/master
         createTable(tableName: "quote") {
             column(autoIncrement: "true", name: "id", type: "BIGINT") {
                 constraints(primaryKey: "true", primaryKeyName: "quotePK")
